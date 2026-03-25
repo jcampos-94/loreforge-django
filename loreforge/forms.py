6079261622297dfa5de551bfef489a14bb4820fb
jcampos-94 <jcampos.94@hotmail.com>
2026-03-25 18:17:20 -0500

    mentor dropdown

--- loreforge/forms.py
+++ loreforge/forms.py
@@ -24,6 +24,25 @@ class CharacterForm(forms.ModelForm):
         model = Character
         fields = ["name", "role", "faction", "mentor"]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # No mentor option until faction is selected
+        self.fields["mentor"].queryset = Character.objects.none()
+
+        if "faction" in self.data:
+            try:
+                faction_id = int(self.data.get("faction"))
+                self.fields["mentor"].queryset = Character.objects.filter(
+                    faction_id=faction_id
+                )
+            except (ValueError, TypeError):
+                pass
+        elif self.instance.pk:
+            # For editing existing character
+            # Ensure only mentors from same faction show
+            self.fields["mentor"].queryset = self.instance.faction.members.all()
+
     def clean_name(self):
         name = self.cleaned_data["name"]
         return format_name(name)
